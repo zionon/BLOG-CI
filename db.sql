dd51e0f556aafc9e786c1f09b6f2a4980b31e74f
gh-huang <gh_huang@outlook.com>
2016-06-23 11:08:55 +0800

    用户表建表

--- db.sql
+++ db.sql
@@ -13,3 +13,13 @@ create table ci_post
 	update_time int(11) default null comment '最后一次修改时间',
 	primary key (id)
 )engine=InnoDB default charset=utf8 collate=utf8_unicode_ci comment '日志';
+
+drop table if exists ci_user
+create table ci_user
+(
+	id int(11) not null auto_increment comment 'Id',
+	username varchar(128) collate utf8_unicode_ci not null comment '用户名',
+	password varchar(128) collate utf8_unicode_ci not null comment '密码',
+	email varchar(128) collate utf8_unicode_ci not null comment '注册邮箱',
+	primary key (id) 
+)engine=InnoDB default charset=utf8 collate=utf8_unicode_ci comment '用户表';
