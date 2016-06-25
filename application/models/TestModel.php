d0d33890b1982dd359f91c25479c6adc9dcf90bc
gh-huang <gh_huang@outlook.com>
2016-06-25 23:12:57 +0800

    封装author_id随机生成

--- application/models/TestModel.php
+++ application/models/TestModel.php
@@ -3,34 +3,14 @@ class TestModel extends MY_Model
 {
 	protected $_tableName = 'post';
 	public function create() {
-		$titleTotal = rand(4,10);
-		$titleNum = array();
-		$titleString = '';
-		for ($i=0; $i < $titleTotal; $i++) { 
-			// $ds = rand(1,23940);
-			// $hex = dechex($ds);
-			$titleNum[$i] = rand(1,23940);
-		}
-		// $titleNum = implode(',',$titleNum);
-		// var_dump($titleNum);die;
-		$this->db->from('ci_cp936');
-		$this->db->select('cjk');
-		$this->db->where_in('id',$titleNum);
-		$unicode = $this->db->get();
-		// $str = $this->db->last_query();
-		// var_dump($str);die;
-		// $titleString = "&#x8FD9;&#x662F;&#x6807;&#x9898;&#xFF0C;&#x8FD9;&#x662F;&#x5185;&#x5BB9;";
-		// var_dump($unicode->result());die;
-		foreach ($unicode->result() as $value) {
-			$titleString .= '&#x'.$value->cjk.';';
-		}
-		$titleString = html_entity_decode($titleString); 
+		$titleString = $this->title(4,10);
+		$authorId = $this->authorId(1,7);
 		$data = array();
 		$data['title'] = $titleString;
 		$data['content'] = '美国，人类的希望';
 		$data['create_time'] = time();
 		$data['update_time'] = time();
-		$data['author_id'] = 3;
+		$data['author_id'] = $authorId;
 		$this->db->insert($this->_tableName, $data);
 		return $data['id'] = $this->db->insert_id();
 	}
@@ -51,4 +31,30 @@ class TestModel extends MY_Model
 		$titleString = html_entity_decode($titleString);
 		return $titleString;
 	}
+
+	private function createTime() {
+
+	}
+
+	private function authorId($min, $max) {
+		$authorId = rand($min, $max);
+		return $authorId;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
