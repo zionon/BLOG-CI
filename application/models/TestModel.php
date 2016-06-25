8cc5dbb13f8fe425b3aa1adb9028ce53a73c79d8
gh-huang <gh_huang@outlook.com>
2016-06-25 22:01:14 +0800

    设置随机取目录数组

--- application/models/TestModel.php
+++ application/models/TestModel.php
@@ -4,15 +4,16 @@ class TestModel extends MY_Model
 	protected $_tableName = 'post';
 	public function create() {
 		$titleTotal = rand(4,10);
-		$titleString = '&#x0101';
-		// for ($i=0; $i < $titleTotal; $i++) { 
-		// 	$ds = rand(14989440,15318688);
-		// 	$hex = dechex($ds);
-		// 	$titleString .= '&#x'.$hex.';';
-		// }
+		$titleNum = array();
+		$titleString = '';
+		for ($i=0; $i < $titleTotal; $i++) { 
+			// $ds = rand(1,23940);
+			// $hex = dechex($ds);
+			$titleNum[$i] = rand(1,23940);
+		}
 
-		// // $titleString = "&#x8FD9;&#x662F;&#x6807;&#x9898;&#xFF0C;&#x8FD9;&#x662F;&#x5185;&#x5BB9;";
-		// var_dump($titleString);
+		// $titleString = "&#x8FD9;&#x662F;&#x6807;&#x9898;&#xFF0C;&#x8FD9;&#x662F;&#x5185;&#x5BB9;";
+		var_dump($titleNum);die;
 		$titleString = html_entity_decode($titleString); 
 		var_dump($titleString);die;
 		// $test = 0b111001001011100010100101;
