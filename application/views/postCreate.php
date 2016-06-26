4a4f8d0bb1958e235119b6cf8ed8c8e3a00acd96
gh-huang <gh_huang@outlook.com>
2016-06-26 23:30:43 +0800

    引入另一个富文本编辑器wangEditor

--- application/views/postCreate.php
+++ application/views/postCreate.php
@@ -12,7 +12,7 @@
 		<div class="post-form">
 			<form id="w0" action="<?=site_url('PostController/postCreate')?>" method="post">
 		<!-- 		<input type="hidden" name="_csrf" value="RDB0NmpwNUx1RjFmWxV9Hxx7Hkw6IHc9G14WTg01fykDZx8BHBJmFg==" /> -->
-				<div class="form-group field-post-title required">
+				<div class="form-group field-post-title required" style="width: 30%">
 					<label class="control-label" for="post-title">标题</label>
 					<input type="text" id="post-title" class="form-control" name="Post[title]" maxlength="128" value="<?=set_value('Post[title]')?>" />
 					<?php $error = form_error('Post[title]'); ?>
@@ -24,7 +24,10 @@
 				<div class="form-group field-post-content required">
 					<label class="control-label" for="post-content">内容</label>
 					<!-- <textarea id="post-content" class="form-control" name="Post[content]" rows="6"><?=set_value('Post[content]')?></textarea> -->
-					<textarea id="editor" name="Post[content]" placeholder="Hello world" autofocus></textarea>
+					<!-- <textarea id="editor" name="Post[content]" placeholder="Hello world" autofocus></textarea> -->
+					<textarea id="textarea1" name="Post[content]" style="height: 400px">
+						<p>Hello World....</p>
+					</textarea>
 					<?php $error = form_error('Post[content]'); ?>
 					<span style="color:#F00;font-weight:bold;"><?=$error?></span>
 
@@ -58,7 +61,7 @@
 
 </div>
 <?php $this->load->view('layout/foot'); ?>
-<link rel="stylesheet" type="text/css" href="<?=_PUBLIC?>/simditor-2.3.6/styles/simditor.css" />
+<!-- <link rel="stylesheet" type="text/css" href="<?=_PUBLIC?>/simditor-2.3.6/styles/simditor.css" />
 <script type="text/javascript" src="<?=_PUBLIC?>/simditor-2.3.6/scripts/module.js"></script>
 <script type="text/javascript" src="<?=_PUBLIC?>/simditor-2.3.6/scripts/hotkeys.js"></script>
 <script type="text/javascript" src="<?=_PUBLIC?>/simditor-2.3.6/scripts/uploader.js"></script>
@@ -67,4 +70,36 @@
 	var editor = new Simditor({
 		textarea: $('#editor')
 	});
+</script> -->
+
+<!--引入wangEditor.css-->
+<link rel="stylesheet" type="text/css" href="<?=_PUBLIC?>/wangEditor-2.1.13/dist/css/wangEditor.min.css">
+
+<!--引入jquery和wangEditor.js-->   <!--注意：javascript必须放在body最后，否则可能会出现问题-->
+<script type="text/javascript" src="<?=_PUBLIC?>/wangEditor-2.1.13/dist/js/wangEditor.min.js"></script>
+<script type="text/javascript">
+	var editor = new wangEditor('textarea1');
+	editor.create();
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
