d9f0e76b301cedd33ff8146aa5dd75cbf9bde6d0
gh-huang <gh_huang@outlook.com>
2016-06-27 11:40:21 +0800

    建表状态表

--- application/views/postUpdate.php
+++ application/views/postUpdate.php
@@ -21,9 +21,21 @@
 					<div class="help-block"></div>
 				</div>
 
-				<div class="form-group field-post-content required">
+<!-- 				<div class="form-group field-post-content required">
 					<label class="control-label" for="post-content">内容</label>
 					<textarea id="post-content" class="form-control" name="Post[content]" rows="6"><?=set_value('Post[content]',$content)?>
+					</textarea>
+					<?php $error = form_error('Post[content]'); ?>
+					<span style="color:#F00;font-weight:bold;"><?=$error?></span>
+
+					<div class="help-block"></div>
+				</div> -->
+				<div class="form-group field-post-content required">
+					<label class="control-label" for="post-content">内容</label>
+					<!-- <textarea id="post-content" class="form-control" name="Post[content]" rows="6"><?=set_value('Post[content]')?></textarea> -->
+					<!-- <textarea id="editor" name="Post[content]" placeholder="Hello world" autofocus></textarea> -->
+					<textarea id="textarea1" name="Post[content]" style="height: 400px">
+						<p><?=set_value('Post[content]', $content)?></p>
 					</textarea>
 					<?php $error = form_error('Post[content]'); ?>
 					<span style="color:#F00;font-weight:bold;"><?=$error?></span>
@@ -59,3 +71,15 @@
 
 </div>
 <?php $this->load->view('layout/foot'); ?>
+
+<!--引入wangEditor.css-->
+<link rel="stylesheet" type="text/css" href="<?=_PUBLIC?>/wangEditor-2.1.13/dist/css/wangEditor.min.css">
+
+<!--引入jquery和wangEditor.js-->   <!--注意：javascript必须放在body最后，否则可能会出现问题-->
+<script type="text/javascript" src="<?=_PUBLIC?>/wangEditor-2.1.13/dist/js/wangEditor.min.js"></script>
+<script type="text/javascript">
+	var editor = new wangEditor('textarea1');
+	editor.config.uploadImgUrl = 'http://www.blog.com/public/wangEditor-2.1.13/uploadfiles/upload.php';
+	editor.config.uploadImgFileName = 'file'
+	editor.create();
+</script>
--- db.sql
+++ db.sql
@@ -26,3 +26,22 @@ create table ci_user
 	email varchar(128) collate utf8_unicode_ci not null comment '注册邮箱',
 	primary key (id) 
 )engine=InnoDB default charset=utf8 collate=utf8_unicode_ci comment '用户表';
+
+drop table if exists ci_lookup
+create table ci_lookup
+(
+	id int(11) not null auto_increment comment 'Id',
+	name varchar(128) collate utf8_unicode_ci not null comment '状态名字',
+	code int(11) not null,
+	primary key (id)
+)engine=InnoDB default charset=utf8 collate=utf8_unicode_ci comment'评论以及日志状态表';
+
+
+
+
+
+
+
+
+
+
